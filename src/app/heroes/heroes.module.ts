f50fe0ddfa3c0bbff7fb43b1dac5cc061c147e1b
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-13 23:54:48 -0600

    agregar sidenav

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,6 +9,7 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ListadoComponent } from './pages/listado/listado.component';
   imports: [
     CommonModule,
     FlexLayoutModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }

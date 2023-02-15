9a7df184d21ee84fa5e7299c4a315541529a6be2
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-15 00:30:11 -0600

    cargar info en heroe component

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { delay, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-heroe',
@@ -8,11 +11,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HeroeComponent implements OnInit{
 
-  constructor(private activatedRoute: ActivatedRoute){}
+  heroe!: Heroe;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService,
+    private router: Router
+  ){}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({id}) => console.log(id));
+    this.activatedRoute.params.pipe(
+      delay(1000),
+      switchMap(({id}) => this.heroesService.getHeroesById(id))
+    )
+    .subscribe(heroe => this.heroe = heroe);
     // this.activatedRoute.params.subscribe(console.log);
+    
+  }
+
+  regresar(){
+    this.router.navigateByUrl('/heroes');
   }
 
 }

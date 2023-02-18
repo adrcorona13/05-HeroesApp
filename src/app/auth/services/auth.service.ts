afe1b355cfdfeee1d494d4cebc43c53766f8403f
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-17 22:27:37 -0600

    agregar logica basica para login y logout

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Auth } from '../interfaces/auth.interfaces';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  get auth(): Auth{
+    return {...this._auth!};
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  login():Observable<Auth>{
+    return this.httpClient.get<Auth>(`${this.baseUrl}/usuarios/1`)
+      .pipe(
+        tap(auth => {
+          this._auth = auth;
+          
+        })
+      );  
+  }
+}
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Auth } from 'src/app/auth/interfaces/auth.interfaces';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  get auth(){
+    return this.authService.auth;
+  }
+
+  constructor(private router: Router,
+              private authService: AuthService){}
+
+  logout(){
+    this.router.navigate(['./auth']);
+  }
 }
